598e21e84dea336bd00e730aa3185216e1126aac
mauro <maurodbds@gmail.com>
2026-04-30 01:00:03 -0300

    Solucao weird

--- CSES/weirdAlgorithm.cpp
+++ CSES/weirdAlgorithm.cpp
@@ -8,17 +8,17 @@ int main() {
 
     cin >> num;
 
+    cout << num << " ";
     while(num != 1){
-        cout << num << " ";
         if(num % 2 == 0){
             num = num/2;
         }
         else{
             num = num * 3 + 1;
         }
+        cout << num << " ";
 
     }
 
-    cout << "1";
     return 0;
 }
